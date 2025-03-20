3488b394fdd21350349ec81911e49bd3c198fe97
IshitaSadaria <ishitasadaria84308@gmail.com>
2025-03-20 14:57:05 +0530

    swap functionality

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,14 @@ function App() {
   const currencyInfo = useCurrencyInfo(from)
   const options = Object.keys(currencyInfo)
 
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+
+  
   return (
     <>
       <h1 className="text-3xl bg-orange-500 text-white text-center mt-2">Currency Convertor</h1>
